f30e73a76d8d1fe649be6dffe953502b8774b065
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-23 22:57:21 +0530

    updated

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,7 +1,13 @@
 export const userController = {
     login(request,respose){
-        const body = request.body;
-        console.log('request body is',body);
+        const userInfo = request.body;
+       // eslint-disable-next-line no-cond-assign
+       if(userInfo.userid = userInfo.password){
+        respose.json({message:'welcome'+ userInfo.userid});
+       }else{
+        respose.json({message:'INVALID'})
+       }
+        // console.log('request body is',body);
         respose.json({message:'Login'})
     },
     register(request,respose){
--- src/db/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const url = 'mongodb+srv://admin:admin123@cluster0.1ldweh5.mongodb.net/?retryWrites=true&w=majority';
-const promise = mongoose.connect(url);
-promise.then(data=>{
-    console.log('DB COnnected...');
-}).catch(err=>{
-    console.log('Error during DB Connection',err);
-})
