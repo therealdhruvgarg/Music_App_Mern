8683bd254d947869490aa4a666428fff4fbf9691
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-24 00:37:13 +0530

    updated

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -3,14 +3,18 @@ import { hashing } from "../utils/encrypt.js";
 export const userController = {
     login(request,respose){
         const userInfo = request.body;
-       // eslint-disable-next-line no-cond-assign
-       if(userInfo.userid === userInfo.password){
-        respose.json({message:'welcome '+ userInfo.userid});
-       }else{
-        respose.json({message:'INVALID'})
-       }
-        // console.log('request body is',body);
-        respose.json({message:'Login'})
+        const doc = usermodel.findOne({'email':userInfo.email}).exec();
+        if(doc&&doc._id){
+            const plainPassword = userInfo.password;
+            const dbpassword = doc.password;
+            if(hashing.matchpassword(plainPassword,dbpassword)){
+                respose.json({message:"Welsome "+doc.name})
+            }else{
+                respose.json({message:"INVALID USERID ORhPASSWORD"});
+            }
+        }else{
+            respose.json({message:"INVALID USERID OR PASSWORD"});
+        }
     },
     async register(request,respose){
         const userInfo =request.body;
@@ -18,22 +22,22 @@ export const userController = {
         try{
         const doc = await usermodel.create(userInfo);
         if(doc && doc._id){
-        respose.json({message:'Register Successful'});
+        respose.json({message:"'Register Successful'"});
         }else{
-            respose.json({message:'Unsuccessful'});
+            respose.json({message:"'Unsuccessful'"});
         }
        
         } 
         catch(err){
             console.log('Register Error ', err)};
-            respose.json({message:'Unsuccessful'});
+            respose.json({message:"'ERRUnsuccessful'"});
     },
     profile(request,respose){
         const userName  = request.params.username;
         console.log('All params',userName)
-        respose.json({message:userName+' Profile'})
+        respose.json({message:userName+"' Profile'"})
     },
     changePassword(request,respose){
-        respose.json({message:'ChangePassword'})
+        respose.json({message:"'ChangePassword'"})
     }
 }
--- backend/utils/encrypt.js
+++ backend/utils/encrypt.js
@@ -2,6 +2,9 @@ import bcrypt from 'bcrypt';
 export const hashing = {
     SALT : 10,
     passwordHash(plainPwd){
-        bcrypt.hashSync(plainPwd,this.SALT);
+        return bcrypt.hashSync(plainPwd,this.SALT);
+    },
+    matchpassword(plainpwd,dbpwd){
+        return bcrypt.compareSync(plainpwd,dbpwd)
     }
 }
