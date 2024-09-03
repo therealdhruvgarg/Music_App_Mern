bfe3f0e624c444d53f341ab8a3984a46c30cb219
Dhruv Garg <dhruv.g4445@gmail.com>
2024-09-03 20:39:58 +0530

    updated

---
+++ app/components/AppBar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function AppBar() {
+  const session = useSession();
+    return (
+    <div>
+      <div className="flex justify-between">
+        <div>Yohoho Music</div>
+        <div>
+            {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>SignOut</button>}
+            {!session.data?.user &&  <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button>}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+import Image from "next/image";
+import { AppBar } from "./components/AppBar";
+
+export default function Home() {
+  return (
+    <main >
+      <AppBar>
+
+      </AppBar>
+    </main>
+  );
+}
