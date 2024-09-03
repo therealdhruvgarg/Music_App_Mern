1574a6e2813a71aedb8da3fee9cbdbbc70ab4b11
Dhruv Garg <dhruv.g4445@gmail.com>
2024-09-03 19:56:52 +0530

    chore: Remove unused files and dependencies

--- backend/db/connection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const url = 'mongodb+srv://admin:admin123@cluster0.1ldweh5.mongodb.net/userdb?retryWrites=true&w=majority';
-const promise = mongoose.connect(url);
-promise.then(data=>{
-    console.log('DB COnnected...');
-}).catch(err=>{
-    console.log('Error during DB Connection',err);
-})
-export default mongoose;
--- backend/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-import express  from "express";
-import {userController} from '../controllers/user-controller.js';
-const userRoutes = express.Router();
-userRoutes.get('/profile/:username',userController.profile)
-userRoutes.post('/login',userController.login)
-userRoutes.post('/registration',userController.register)
-// userRoutes.put('/change-password')
-// userRoutes.delete('/remove-account');
-export default userRoutes;
