b6a4e3422c45cff7952d7efa971e582f7cc8ec8f
Dhruv Garg <dhruv.g4445@gmail.com>
2024-09-05 01:21:42 +0530

    feat: Add API routes for upvoting and downvoting streams

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,35 +1,47 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import {z} from "zod";  
-const YT_REGEX = new RegExp("^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/(watch\?v=)?[a-zA-Z0-9_-]{11}$")
+import { z } from "zod";
+const YT_REGEX = new RegExp(
+  "^(https?://)?(www.youtube.com|youtu.be)/(watch?v=)?[a-zA-Z0-9_-]{11}$"
+);
 
 const CreateStreamSchema = z.object({
-    creatorId: z.string(),
-    url: z.string()
+  creatorId: z.string(),
+  url: z.string(),
 });
 
-export async function POST(req:NextRequest){
-   try {const data = CreateStreamSchema.parse( await req.json());
+export async function POST(req: NextRequest) {
+  try {
+    const data = CreateStreamSchema.parse(await req.json());
     const isYt = YT_REGEX.test(data.url);
-    if(!isYt){
-        return NextResponse.json({
-            message : "Wrong URL"
-        ,},{
-            status : 411
-        })
+    if (!isYt) {
+      return NextResponse.json(
+        {
+          message: "Wrong URL",
+        },
+        {
+          status: 411,
+        }
+      );
     }
-const extractedId = data.url.split("?v=")[1];
+    const extractedId = data.url.split("?v=")[1];
 
-    prismaClient.stream.create({
-        data:{userId: data.creatorId,
-        url :data.url,extractedId,type:"Youtube"}
-   })}
-   catch(e){
-    return NextResponse.json({
-        message : "Error while adding the stream"
-    ,},{
-        status : 411
-    })
-   }
-
+    await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type: "Youtube",
+      },
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "Error while adding the stream",
+      },
+      {
+        status: 411,
+      }
+    );
+  }
 }
