8a0288869c761584fca64fb1c27bc108a5768a27
Dhruv Garg <dhruv.g4445@gmail.com>
2024-09-06 13:12:41 +0530

    feat: Add Redirect component for handling session redirection
    - Added a new component called Redirect to handle session redirection
    - The Redirect component uses the useSession hook from next-auth/react and the useRouter hook from next/navigation
    - When a user is logged in, the Redirect component automatically redirects them to the dashboard page
    - This component is useful for controlling the flow of the application based on the user's session status

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Music, Users, Radio, Headphones } from "lucide-react"
+import { Users, Radio, Headphones } from "lucide-react"
 import Link from "next/link"
 import { AppBar } from "./components/AppBar"
+import { Redirect } from "./components/Redirect"
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-gray-100">
      
         <AppBar/>
-     
+      <Redirect/>
       <main className="flex-1 py-12 px-4">
         <div className="container mx-auto max-w-3xl text-center">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl mb-6 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
